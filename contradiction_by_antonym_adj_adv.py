dfd6755f75b418512125d34f14c9f5e80efaa95f
schumanng <55746316+schumanng@users.noreply.github.com>
2022-12-17 11:59:46 +0100

    Update contradiction_by_antonym_adj_adv.py

--- contradiction_by_antonym_adj_adv.py
+++ contradiction_by_antonym_adj_adv.py
@@ -176,7 +176,7 @@ def antonym_adj_adv(nlp_model, sent):
 
 
 # load SNLI training set
-train_df = pd.read_csv('snli_1.0_train.csv', sep=',')  # adjust the path according to your directory
+train_df = pd.read_csv('input/snli_1.0_train.csv', sep=',')  # adjust the path according to your directory
 train_df_sample = train_df#[:2000]  # draw a sample, if desired
 train_df_sample = train_df_sample['sentence1']
 train_df_sample = train_df_sample.drop_duplicates()
